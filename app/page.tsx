8267e0a030f1a69b790b363ec1398990fcd14e14
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-10-11 01:19:36 -0300

    feat: configured functions and herocontent

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "@/components/main/Hero";
+
 export default function Home() {
   return (
-    <main>
-      space portfolio
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
     </main>
   );
 }
---
+++ components/main/Hero.tsx
@@ -0,0 +1,19 @@
+import HeroContent from "../sub/HeroContent";
+
+const Hero = () => {
+    return ( 
+        <div className="relative flex flex-col h-full w-full">
+            <video
+            autoPlay
+            muted
+            loop //COLOCAR -140px NO TOP PARA AJUSTAR O VÍDEO
+            className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover" 
+            >
+                <source src="./blackhole.webm" type="video/webm" />
+            </video>
+            <HeroContent />
+        </div>
+     );
+}
+ 
+export default Hero;
