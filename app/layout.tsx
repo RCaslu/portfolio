8fa0d89becb5ce1ed29acf84cadc92b8deda99e0
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-10-14 08:50:48 -0300

    feat: added NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
+import { Navbar } from "@/components/main/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas />
+        <Navbar />
         {children}
       </body>
     </html>
